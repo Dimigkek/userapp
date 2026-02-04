77489d4157663932af4665dc38ff97043e127f32
dimigkek <dimigkek@gmail.com>
2026-02-04 04:34:20 +0200

    Added Serviceimpls

---
+++ userapp/src/main/java/com/example/userapp/service/impl/AddressServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.userapp.service.impl;
+
+import com.example.userapp.entity.Address;
+import com.example.userapp.repository.AddressRepository;
+import com.example.userapp.service.AddressService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressServiceImpl(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public Address save(Address address) {
+        return addressRepository.save(address);
+    }
+
+    @Override
+    public List<Address> findAll() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public Optional<Address> findById(Long id) {
+        return addressRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        addressRepository.deleteById(id);
+    }
+}
---
+++ userapp/src/main/java/com/example/userapp/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.userapp.service.impl;
+
+import com.example.userapp.entity.User;
+import com.example.userapp.repository.UserRepository;
+import com.example.userapp.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
