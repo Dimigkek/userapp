1ba2ee01b26bb5c48ba50eb52ff833e627188cb5
dimigkek <dimigkek@gmail.com>
2026-02-05 04:15:48 +0200

    Fix 2

--- userapp/src/main/java/com/example/userapp/repository/UserRepository.java
+++ userapp/src/main/java/com/example/userapp/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.userapp.repository;
 import com.example.userapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
