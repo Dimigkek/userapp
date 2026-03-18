9eeb29d71cbca6d827c3c7ff0bd7198d0ece1e5a
Gkekas Dimitris <dimigkek@gmail.com>
2026-03-18 15:54:25 +0200

    Add @Repository annotation to AddressRepository

--- userapp/src/main/java/com/example/userapp/repository/AddressRepository.java
+++ userapp/src/main/java/com/example/userapp/repository/AddressRepository.java
@@ -3,5 +3,6 @@ package com.example.userapp.repository;
 import com.example.userapp.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
